6d694cb17281732e4def7ff7fae9b5ee9196ecbb
babycarrot1211 <73012025+wongjingen1211@users.noreply.github.com>
2021-08-18 00:44:26 +0800

    Reset the navigation
    change 'EditCategory' to 'CategoryScreen'.

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -1,57 +1,44 @@
-import React, { Component } from 'react';
-import {
-    StyleSheet,
-    View,
-    Text,
-    TextInput,
-    Alert,
-    Button,
-} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, View, Text, TextInput, Alert, Button} from 'react-native';
 import SettingsList from 'react-native-settings-list';
 
-const SettingScreen = ({ navigation }) => {
+const SettingScreen = ({navigation}) => {
+  return (
+    <View style={{backgroundColor: '#EFEFF4', flex: 1}}>
+      <View style={{backgroundColor: '#EFEFF4', flex: 1}}>
+        <SettingsList borderColor="#c8c7cc" defaultItemSize={50}>
+          <SettingsList.Header headerStyle={{marginTop: 15}} />
 
-
-    return (
-        <View style={{ backgroundColor: '#EFEFF4', flex: 1 }}>
-
-            <View style={{ backgroundColor: '#EFEFF4', flex: 1 }}>
-                <SettingsList borderColor='#c8c7cc' defaultItemSize={50}>
-                    <SettingsList.Header headerStyle={{ marginTop: 15 }} />
-
-                    <SettingsList.Item
-                        title='Reminder'
-                        titleInfoStyle={styles.titleInfoStyle}
-                        onPress={() => navigation.navigate("Reminder")}
-                    />
-                    <SettingsList.Item
-                        title='Edit Categories'
-                        titleInfoStyle={styles.titleInfoStyle}
-                        onPress={() => navigation.navigate("EditCategories")}
-                    />
-                    <SettingsList.Header headerStyle={{ marginTop: 15 }} />
-                    <SettingsList.Item
-                        title='MonthlyBudgets'
-                        onPress={() => navigation.navigate("MonthlyBudgets")}
-                    />
-
-                </SettingsList>
-            </View>
-        </View>
-    );
-
-
-}
+          <SettingsList.Item
+            title="Reminder"
+            titleInfoStyle={styles.titleInfoStyle}
+            onPress={() => navigation.navigate('Reminder')}
+          />
+          <SettingsList.Item
+            title="Categories"
+            titleInfoStyle={styles.titleInfoStyle}
+            onPress={() => navigation.navigate('CategoryScreen')}
+          />
+          <SettingsList.Header headerStyle={{marginTop: 15}} />
+          <SettingsList.Item
+            title="MonthlyBudgets"
+            onPress={() => navigation.navigate('MonthlyBudgets')}
+          />
+        </SettingsList>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    imageStyle: {
-        marginLeft: 15,
-        marginRight: 20,
-        alignSelf: 'center',
-        width: 20,
-        height: 24,
-        justifyContent: 'center'
-    }
+  imageStyle: {
+    marginLeft: 15,
+    marginRight: 20,
+    alignSelf: 'center',
+    width: 20,
+    height: 24,
+    justifyContent: 'center',
+  },
 });
 
 export default SettingScreen;
