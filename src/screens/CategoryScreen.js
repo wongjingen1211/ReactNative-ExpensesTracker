b92e9426653605bab66515f0c47acfbfa9a26f7f
babycarrot1211 <73012025+wongjingen1211@users.noreply.github.com>
2021-08-18 00:47:50 +0800

    Remake View Category Screen
    1. Make all category display in FlatList, so they can be increase if user insert a new category. Also, if use the format of icons, requires more storages and effort to implement.
    2. Click into a category will directly navigate to 'EditCategoryScreen'.
    3. The 'FloatingAction' only remain 'add' function.

--- src/screens/CategoryScreen.js
+++ src/screens/CategoryScreen.js
@@ -1,61 +1,129 @@
-import React, { useState } from "react";
-import { Text, StyleSheet, TouchableOpacity, View, SafeAreaView} from "react-native";
+import React, {useState, Component} from 'react';
+import {
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+  Alert,
+  TouchableHighlight,
+  FlatList,
+} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Icon from "react-native-vector-icons/Ionicons";
-import CategoryButton from "../../component/CategoryButton";  
-import { bold } from "jest-matcher-utils/node_modules/chalk";
-import { styles } from "ansi-colors";
-import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import CategoryButton from '../../component/CategoryButton';
+import {bold} from 'jest-matcher-utils/node_modules/chalk';
+import {styles} from 'ansi-colors';
+import {NavigationContainer} from '@react-navigation/native';
+import {FloatingAction} from 'react-native-floating-action';
 
+const actions = [
+  {
+    text: 'Add',
+    icon: require('../../assets/icons/create.png'),
+    name: 'add',
+    position: 1,
+  },
+];
 
-const CategoryScreen = ({navigation}) => {
-  let A = ()=>navigation.navigate("Expense")
-  return(
-    <SafeAreaView style= {styles2.Space}>
-      <Text style={styles2.text}>
-        Please choose the category you want to record.
-      </Text>
-      <View style={styles2.Parent}>
-        <CategoryButton text={"Food"} icon={'food'} D={A} />
-        <CategoryButton text={"Daily Necessity"} icon={"toothbrush"}  D={A}/>
-        <CategoryButton text={"Entertainment"} icon={"movie"}  D={A}/>
-      </View>
-      <View style={styles2.Parent}>
-        <CategoryButton text={"Books"} icon={"book-open-page-variant"}  D={A}/>
-        <CategoryButton text={"Insurance"} icon={"shield-car"} D={A}/>
-        <CategoryButton text={"Loan"} icon={"currency-usd"}  D={A}/>
-      </View>
-      <View style={styles2.Parent}>
-        <CategoryButton text={"Others"} icon={"dots-horizontal"} D={A}/>
-      </View>
-      
-     </SafeAreaView>
-  );
-  
-    
-  
-};
+type Props = {};
+export default class EditCategories extends Component {
+  static navigationOptions = {
+    title: 'City List',
+  };
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      categories: [],
+      isFetching: false,
+    };
 
+    this._selectAllCategory = this._selectAllCategory.bind(this);
+  }
+
+  componentDidMount() {
+    this._selectAllCategory();
+  }
+
+  _selectAllCategory() {
+    //fetch all category in this function
+    let url = 'http://192.168.1.192:5000/api/category';
+    this.setState({isFetching: true});
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          //catch the responds error status
+          Alert.alert('Error', response.status.toString());
+          throw Error('Error ' + response.status);
+        }
+        //respond status e.g., 201 created, 202 OK, 404 no found
+        return response.json();
+      })
+      //after places from server successfully, take the table places as i nput for further execution.
+      .then(categories => {
+        this.setState({categories});
+        //update the dictionary
+        this.setState({isFetching: false});
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <SafeAreaView style={styles2.Space}>
+        <FlatList
+          data={this.state.categories}
+          extraData={this.state}
+          showsVerticalScrollIndicator={true}
+          renderItem={({item}) => {
+            return (
+              <TouchableHighlight
+                underlayColor={'#cccccc'}
+                onPress={() => {
+                  this.props.navigation.navigate('EditCategories', {
+                    id: item.category_id,
+                    name: item.category_name,
+                    refresh: this._selectAllCategory,
+                  });
+                }}>
+                <Text style={styles.text}>{item.category_name}</Text>
+              </TouchableHighlight>
+            );
+          }}
+          keyExtractor={item => {
+            item.category_id.toString();
+          }}
+        />
+        <FloatingAction
+          actions={actions}
+          color={'lightblue'}
+          onPressItem={() => {
+            this.props.navigation.navigate('Reminder', {
+              refresh: this._selectAllCategory,
+            });
+          }}
+        />
+      </SafeAreaView>
+    );
+  }
+}
 
 const styles2 = StyleSheet.create({
-  Space:{
+  Space: {
     flex: 1.0,
-    
   },
-  Parent:{
+  Parent: {
     flex: 1.0,
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop:10,
+    marginTop: 10,
   },
-  text:{
+  text: {
     fontSize: 18,
     fontWeight: 'bold',
     color: 'red',
-    marginLeft: 5
-  }
-
+    marginLeft: 5,
+  },
 });
-
-export default CategoryScreen;
