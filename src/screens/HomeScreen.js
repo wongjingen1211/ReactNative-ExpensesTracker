9737d05e7aae0f54cb18604acec42de715a1e262
babycarrot1211 <73012025+wongjingen1211@users.noreply.github.com>
2021-08-19 14:22:00 +0800

    Add the function to view 'Selected Day Transaction'
    The 'Select Date' button is just temporary, assumes the final product is a fully opened calendar at the top, after user selects a date, then the selected transactions will be listed at the bottom, UI team please help to make it prettier.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'react-native-gesture-handler';
 import {
   StyleSheet,
@@ -7,22 +7,193 @@ import {
   View,
   TouchableOpacity,
   SafeAreaView,
+  TouchableHighlight,
+  FlatList,
+  DatePickerAndroid,
+  Alert,
 } from 'react-native';
+import {AppButton} from './UI';
 import {NavigationContainer} from '@react-navigation/native';
 
-const HomeScreen = ({navigation}) => {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={{flex: 1, padding: 16}}>
-        <TouchableOpacity
+type Props = {};
+export default class HomeScreen extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected_date: Date.parse(new Date()),
+      selected_transactions: [],
+      isFetching: false,
+    };
+
+    this._selectTransactions = this._selectTransactions.bind(this);
+  }
+
+  componentDidMount() {
+    this._selectTransactions();
+  }
+
+  _selectTransactions() {
+    //preprocess the date format----------
+    var selectedDate = this.state.selected_date; //get the current date
+
+    var start_day = new Date(selectedDate).getDate();
+    var start_month = new Date(selectedDate).getMonth();
+    var start_year = new Date(selectedDate).getFullYear();
+    let monthsText = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    start_month = monthsText[start_month];
+    console.log(start_month + ' ' + start_day + ',' + start_year);
+    let startTime_parsed = Date.parse(
+      start_month + ' ' + start_day + ',' + start_year,
+    ); //(data type: integer)the 12:00am of the CURRENT day
+    let endTime_parsed = startTime_parsed + 86400000; //(data type: integer)the 12:00am of the NEXT day
+
+    console.log(
+      'Viewing Transactions from:' +
+        new Date(startTime_parsed) +
+        ' to ' +
+        new Date(endTime_parsed),
+    );
+    console.log(startTime_parsed);
+    console.log(endTime_parsed);
+    //preprocess the date format----------
+    let url =
+      'http://192.168.1.192:5000/api/transaction/all/' +
+      startTime_parsed +
+      '/' +
+      endTime_parsed;
+    this.setState({isFetching: true});
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          //catch the responds error status
+          Alert.alert('Error', response.status.toString());
+          throw Error('Error ' + response.status);
+        }
+        //respond status e.g., 201 created, 202 OK, 404 no found
+        return response.json();
+      })
+      //after places from server successfully, take the table places as i nput for further execution.
+      .then(selected_transactions => {
+        this.setState({selected_transactions});
+        //update the dictionary
+        this.setState({isFetching: false});
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  openDatePicker = async () => {
+    try {
+      const {action, year, month, day} = await DatePickerAndroid.open({
+        minDate: new Date(2000, 0, 1),
+        maxDate: new Date(2099, 11, 31),
+        mode: 'calendar',
+      });
+      if (action !== DatePickerAndroid.dismissedAction) {
+        // Selected year, month (0-11), day
+        let selectedDate = new Date(year, month, day);
+        let selectedDatesec = selectedDate.getTime();
+        this.setState({
+          selected_date: selectedDatesec,
+        });
+        this._selectTransactions();
+      }
+    } catch ({code, message}) {
+      console.warn('Cannot open date picker', message);
+    }
+  };
+
+  render() {
+    if (this.state.selected_date == 0) var date = new Date();
+    else var date = this.state.selected_date;
+    var dateday = new Date(date).getDate();
+    var month = new Date(date).getMonth();
+    var year = new Date(date).getFullYear();
+    var day = new Date(date).getDay();
+    let daysText = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let monthsText = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ];
+    var dayName = daysText[day];
+    var monthName = monthsText[month];
+    var newdate = dayName + ' ' + dateday + ' ' + monthName + ' ' + year;
+    console.log(this.state.selected_transactions);
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{flex: 1, padding: 16}}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate('Expense')}>
+            <Text>Add Transaction</Text>
+          </TouchableOpacity>
+        </View>
+        <AppButton
           style={styles.button}
-          onPress={() => navigation.navigate('Expense')}>
-          <Text>Add Transaction</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
-};
+          title={'Pick A Date'}
+          theme={'primary'}
+          onPress={this.openDatePicker}
+        />
+
+        <View>
+          <Text>{newdate}</Text>
+          <FlatList
+            data={this.state.selected_transactions}
+            extraData={this.state}
+            showsVerticalScrollIndicator={true}
+            renderItem={({item}) => {
+              return (
+                <TouchableHighlight
+                  underlayColor={'#cccccc'}
+                  onPress={() => {
+                    this.props.navigation.navigate('Category', {
+                      //should navigate to EDIT EXPENSE SCREEN
+                      id: item.amount,
+                      name: item.category_name,
+                      refresh: this._selectAllCategory,
+                    });
+                  }}>
+                  <View style={styles.transaction_row}>
+                    <Text style={styles.text}>{item.amount}</Text>
+                    <Text style={styles.text}>{item.category_name}</Text>
+                  </View>
+                </TouchableHighlight>
+              );
+            }}
+            keyExtractor={item => {
+              item.transaction_id;
+            }}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   button: {
@@ -32,6 +203,16 @@ const styles = StyleSheet.create({
     width: 300,
     marginTop: 16,
   },
+  text: {},
+  transaction_row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: '#000',
+    backgroundColor: '#e7e7e7',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 18,
+  },
 });
-
-export default HomeScreen;
