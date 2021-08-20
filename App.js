10a294338fbc5fdfcac05738b343f2af28db2876
babycarrot1211 <73012025+wongjingen1211@users.noreply.github.com>
2021-08-20 10:58:09 +0800

    Realign, and add route to 'Edit Expense Screen'

--- App.js
+++ App.js
@@ -1,49 +1,52 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { StyleSheet } from "react-native"
+import {StyleSheet} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 
-import Icon from "react-native-vector-icons/Ionicons";
-import HomeScreen from "./src/screens/HomeScreen";
-import ExpenseScreen from "./src/screens/ExpenseScreen";
-import CategoryScreen from "./src/screens/CategoryScreen";
-import SettingScreen from "./src/screens/SettingScreen";
-import AccountReportScreen from "./src/screens/AccountReportScreen";
-import Reminder from "./src/screens/Reminder";
-import EditCategories from "./src/screens/EditCategories";
-import MonthlyBudgets from "./src/screens/MonthlyBudgets";
-import CreateBudget from "./src/screens/CreateBudget";
+import Icon from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './src/screens/HomeScreen';
+import ExpenseScreen from './src/screens/ExpenseScreen';
+import EditExpenseScreen from './src/screens/EditExpenseScreen';
+import CategoryScreen from './src/screens/CategoryScreen';
+import SettingScreen from './src/screens/SettingScreen';
+import AccountReportScreen from './src/screens/AccountReportScreen';
+import Reminder from './src/screens/Reminder';
+import EditCategories from './src/screens/EditCategories';
+import MonthlyBudgets from './src/screens/MonthlyBudgets';
+import CreateBudget from './src/screens/CreateBudget';
 
-import { black } from "color-name";
+import {black} from 'color-name';
 
 const Stack = createStackNavigator();
-
 
 function HomeNavi() {
   return (
     <Stack.Navigator
       initialRouteName="Home"
       screenOptions={{
-        headerStyle: { backgroundColor: '#fffacd' },
+        headerStyle: {backgroundColor: '#fffacd'},
         headerTintColor: '#ff8c00',
-        headerTitleStyle: { fontWeight: 'normal', fontSize: 25 }
+        headerTitleStyle: {fontWeight: 'normal', fontSize: 25},
       }}>
       <Stack.Screen
         name="Home"
         component={HomeScreen}
-        options={{ title: 'Home' }} />
-      <Stack.Screen
-        name="Category"
-        component={CategoryScreen}
-        options={{ title: 'Category' }} />
+        options={{title: 'Home'}}
+      />
       <Stack.Screen
         name="Expense"
         component={ExpenseScreen}
-        options={{ title: 'Expense' }} />
+        options={{title: 'Add Transaction'}}
+      />
+      <Stack.Screen
+        name="EditExpense"
+        component={EditExpenseScreen}
+        options={{title: 'Edit Transaction'}}
+      />
     </Stack.Navigator>
   );
 }
@@ -53,30 +56,40 @@ function SettingNavi() {
     <Stack.Navigator
       initialRouteName="Settings"
       screenOptions={{
-        headerStyle: { backgroundColor: '#fffacd' },
+        headerStyle: {backgroundColor: '#fffacd'},
         headerTintColor: '#ff8c00',
-        headerTitleStyle: { fontWeight: 'normal', fontSize: 25 }
+        headerTitleStyle: {fontWeight: 'normal', fontSize: 25},
       }}>
       <Stack.Screen
         name="Settings"
         component={SettingScreen}
-        options={{ title: 'SettingsPage' }} />
+        options={{title: 'SettingsPage'}}
+      />
       <Stack.Screen
         name="Reminder"
         component={Reminder}
-        options={{ title: 'Reminder' }} />
+        options={{title: 'Reminder'}}
+      />
+      <Stack.Screen
+        name="CategoryScreen"
+        component={CategoryScreen}
+        options={{title: 'CategoryScreen'}}
+      />
       <Stack.Screen
         name="EditCategories"
         component={EditCategories}
-        options={{ title: 'EditCategories' }} />
+        options={{title: 'EditCategories'}}
+      />
       <Stack.Screen
         name="MonthlyBudgets"
         component={MonthlyBudgets}
-        options={{ title: 'Monthly Budgets' }} />
+        options={{title: 'Monthly Budgets'}}
+      />
       <Stack.Screen
         name="CreateBudget"
         component={CreateBudget}
-        options={{ title: 'Create Budget' }} />
+        options={{title: 'Create Budget'}}
+      />
     </Stack.Navigator>
   );
 }
@@ -86,78 +99,70 @@ function SummaryNavi() {
     <Stack.Navigator
       initialRouteName="Summary"
       screenOptions={{
-        headerStyle: { backgroundColor: '#fffacd' },
+        headerStyle: {backgroundColor: '#fffacd'},
         headerTintColor: '#ff8c00',
-        headerTitleStyle: { fontWeight: 'normal', fontSize: 25 }
+        headerTitleStyle: {fontWeight: 'normal', fontSize: 25},
       }}>
       <Stack.Screen
         name="AccountReports"
         component={AccountReportScreen}
-        options={{ title: 'AccountReports' }} />
+        options={{title: 'AccountReports'}}
+      />
     </Stack.Navigator>
   );
 }
 
 const Tab = createBottomTabNavigator();
 
-
 function Menu() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={{
-          "tabBarActiveTintColor": '#dc143c',
-          "tabBarStyle": [
+          tabBarActiveTintColor: '#dc143c',
+          tabBarStyle: [
             {
-              "display": "flex"
+              display: 'flex',
             },
-            null
+            null,
           ],
-          headerShown: false
+          headerShown: false,
         }}
-        initialRouteName="Menu"
-      >
+        initialRouteName="Menu">
         <Tab.Screen
           name="EasyTracker"
           component={HomeNavi}
           options={{
             tabBarLabel: 'Home',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="home"
-                color={color}
-                size={32}
-              />
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="home" color={color} size={32} />
             ),
-          }} />
+          }}
+        />
         <Tab.Screen
           name="EasyTracker2"
           component={SettingNavi}
           options={{
             tabBarLabel: 'Settings',
-            tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons
-                name="cogs"
-                color={color}
-                size={32}
-              />
-
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="cogs" color={color} size={32} />
             ),
-          }} />
+          }}
+        />
         <Tab.Screen
           name="EasyTracker3"
           component={SummaryNavi}
           options={{
             tabBarLabel: 'AccountSummary',
-            tabBarIcon: ({ color, size }) => (
+            tabBarIcon: ({color, size}) => (
               <MaterialCommunityIcons
                 name="account-multiple"
                 color={color}
                 size={32}
               />
-
             ),
-          }} />
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
